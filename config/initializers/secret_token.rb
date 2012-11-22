875905e57fdf3bb3cd7f0b07ad43fb3d93539d4b
Paul Thompson <prtsoft@gmail.com>
2012-11-22 16:40:37 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prtweet::Application.config.secret_token = '47cd145ba8e6a39fed79c9df93991e7d8e876e052c115f71325b90849b73e417a09dedd83f80c404d9b7197101f16b2ffb9b6375064cefb8a608a245c9fdaf7a'
